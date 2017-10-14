417625db3a2aa936aeed832955e69babb20392d6
5120152577 <857165109@qq.com>
2017-10-14 20:07:24 +0800

    第一次

---
+++ IOCPServer/IOCPModel.cpp
@@ -0,0 +1,208 @@
+#include "stdafx.h"
+#include "IOCPModel.h"
+
+
+CIOCPModel::CIOCPModel()
+	:m_nPort(DEFAULT_PORT),
+	 m_numThreads(0),
+	 m_hIOCP(NULL),
+	 m_hQuitEvent(NULL),
+	 m_phWorkerThreads(nullptr),
+	 m_pListenContext(nullptr),
+	 m_lpfnAcceptEx(nullptr),
+	 m_lpfnGetAcceptExSockAddrs(nullptr)
+{
+	//初始化线程互斥量
+	InitializeCriticalSection(&m_csContextList);
+	//建立线程退出事件 默认无信号  不默认重置信号状态
+	m_hQuitEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	//设置服务器地址信息
+	m_serverAddr->sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	m_serverAddr->sin_family = AF_INET;
+	m_serverAddr->sin_port = DEFAULT_PORT;
+}
+
+
+CIOCPModel::~CIOCPModel()
+{
+}
+
+DWORD WINAPI CIOCPModel::WorkerThreadFun(LPVOID lpParam)
+{
+	return 0;
+}
+
+bool CIOCPModel::LoadSocketLab()
+{
+	WSADATA wsaData;
+	//出现错误
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR)
+	{
+		printf("初始化winsock 2.2失败\n");
+		return false;
+	}
+	return true;
+}
+
+bool CIOCPModel::Init()
+{
+	return true;
+}
+
+
+
+bool CIOCPModel::InitIOCP()
+{
+	m_hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+	if (NULL==m_hIOCP)
+	{
+		printf("建立完成端口失败！错误代码：%d\n", WSAGetLastError());
+		return false;
+	}
+	return true;
+}
+
+bool CIOCPModel::InitSocket()
+{
+	// AcceptEx 和 GetAcceptExSockaddrs 的GUID，用于导出函数指针
+	GUID GuidAcceptEx = WSAID_ACCEPTEX;
+	GUID GuidGetAcceptExSockAddrs = WSAID_GETACCEPTEXSOCKADDRS;
+
+	//注意 需要用wsasocket建立
+	m_pListenContext->m_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	if (INVALID_SOCKET == m_pListenContext->m_socket)
+	{
+		printf("初始化socket失败！错误码：%d\n", WSAGetLastError());
+		return false;
+	}
+	//绑定到服务器地址
+	if (SOCKET_ERROR==bind(m_pListenContext->m_socket, (sockaddr *)&m_serverAddr, sizeof(m_serverAddr)))
+	{
+		printf("bind()函数执行错误！\n");
+		RELEASE_SOCKET(m_pListenContext->m_socket);
+		return false;
+	}
+	//绑定至完成端口
+	if (NULL == CreateIoCompletionPort((HANDLE)m_pListenContext->m_socket, m_hIOCP, (DWORD)m_pListenContext, 0))
+	{
+		printf("绑定listen socket至完成端口失败！错误代码：%d\n", WSAGetLastError());
+		RELEASE_SOCKET(m_pListenContext->m_socket);
+		return false;
+	}
+	//开始监听
+	if (SOCKET_ERROR == listen(m_pListenContext->m_socket, 10))
+	{
+		printf("listen()函数执行失败！设置监听失败！错误代码：%d\n", WSAGetLastError());
+		RELEASE_SOCKET(m_pListenContext->m_socket);
+		return false;
+	}
+
+
+	// 使用AcceptEx函数，因为这个是属于WinSock2规范之外的微软另外提供的扩展函数
+	// 所以需要额外获取一下函数的指针，
+	// 获取AcceptEx函数指针
+	DWORD dwBytes = 0;
+	if (SOCKET_ERROR == WSAIoctl(
+		m_pListenContext->m_socket,
+		SIO_GET_EXTENSION_FUNCTION_POINTER,
+		&GuidAcceptEx,
+		sizeof(GuidAcceptEx),
+		&m_lpfnAcceptEx,
+		sizeof(m_lpfnAcceptEx),
+		&dwBytes,
+		NULL,
+		NULL))
+	{
+		DeInit();
+		return false;
+	}
+
+	// 获取GetAcceptExSockAddrs函数指针，也是同理
+	if (SOCKET_ERROR == WSAIoctl(
+		m_pListenContext->m_socket,
+		SIO_GET_EXTENSION_FUNCTION_POINTER,
+		&GuidGetAcceptExSockAddrs,
+		sizeof(GuidGetAcceptExSockAddrs),
+		&m_lpfnGetAcceptExSockAddrs,
+		sizeof(m_lpfnGetAcceptExSockAddrs),
+		&dwBytes,
+		NULL,
+		NULL))
+	{
+		DeInit();
+		return false;
+	}
+
+	//为acceptex准备参数，然后投递io请求
+	for (int i = 0; i < MAX_POST_ACCEPT; i++)
+	{
+		//新建一个io_context
+		PPER_IO_CONTEXT p = m_pListenContext->GetNewIOContext();
+		if (false == PostAccept(p))
+		{
+			m_pListenContext->RemoveContext(p);
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool CIOCPModel::InitWorkerThread()
+{
+	//获得处理器数量
+	SYSTEM_INFO si;
+	GetSystemInfo(&si);
+	int numOfProcessors = si.dwNumberOfProcessors;
+	m_numThreads = THREAD_PER_PROCESSOR*numOfProcessors;
+	//初始化线程
+	m_phWorkerThreads = new HANDLE[numOfProcessors];
+	DWORD nWorkerID;
+	for (int i = 0; i < m_numThreads; i++)
+	{
+		PTHREADPARAM_WORKER param = new THREADPARAM_WORKER;
+		param->m_IOCPModel = this;
+		param->m_noThread = i + 1;
+		m_phWorkerThreads[i] = CreateThread(0, 0, WorkerThreadFun, (LPVOID)param, 0, &nWorkerID);
+	}
+	printf("建立工作者线程 %d个\n", m_numThreads);
+
+
+	return true;
+}
+
+void CIOCPModel::DeInit()
+{
+	//删除线程互斥量
+	DeleteCriticalSection(&m_csContextList);
+	//释放iocp端口句柄
+	RELEASE_HANDLE(m_hIOCP);
+	//关闭事件
+	RELEASE_HANDLE(m_hQuitEvent);
+	//关闭释放线程
+	for (int i = 0; i < m_numThreads; i++)
+	{
+		RELEASE_HANDLE(m_phWorkerThreads[i]);
+	}
+	//删除
+	delete[m_numThreads]m_phWorkerThreads;
+
+	printf("释放资源完毕！\n");
+}
+
+bool CIOCPModel::PostAccept(PPER_IO_CONTEXT p)
+{
+
+	return false;
+}
+
+bool CIOCPModel::PostRecv(PPER_IO_CONTEXT p)
+{
+	return false;
+}
+
+bool CIOCPModel::PostSend(PPER_IO_CONTEXT p)
+{
+	return false;
+}
+
---
+++ IOCPServer/IOCPModel.h
@@ -0,0 +1,162 @@
+#pragma once
+#include <winsock2.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <MSWSock.h>
+using namespace std;
+#pragma comment(lib,"ws2_32.lib")
+
+#define MAX_POST_ACCEPT 10     //初始投递Accept请求的个数
+#define MAX_BUFFLEN   1024     //缓冲区最大长度
+#define DEFAULT_PORT  9990     //默认端口
+#define THREAD_PER_PROCESSOR 2 //一个处理器对应线程的数量
+
+
+#define RELEASE_SOCKET(x) {if((x)!=INVALID_SOCKET){closesocket(x);(x)=INVALID_SOCKET;}}
+#define RELEASE_HANDLE(x) {if((x)!=INVALID_HANDLE_VALUE&&(x)!=NULL){CloseHandle(x);(x)=NULL;}}
+#define RELEASE(x) {if((x)!=nullptr){delete (x);(x)=nullptr;}}
+
+
+
+
+typedef enum _OPERATION_TYPE
+{
+	ACCEPT,                  //有新的客户端连接
+	RECV,                    //有数据从客户端过来
+	SEND,                    //发送数据到客户端
+	INITIALIZE               //初始化
+}OPERATION_TYPE;
+
+//单IO数据
+typedef struct _PER_IO_CONTEXT
+{
+	OVERLAPPED m_overLapped;          //重叠IO
+	OPERATION_TYPE m_type;            //操作类型
+	SOCKET m_socket;                  //socket
+	WSABUF m_wsaBuf;                  //字符缓冲区
+	char m_buffer[MAX_BUFFLEN];       
+
+	_PER_IO_CONTEXT()
+	{
+		memset(&m_overLapped, 0, sizeof(m_overLapped));
+		m_type = INITIALIZE;
+		m_socket = INVALID_SOCKET;
+		memset(m_buffer, 0, sizeof(m_buffer));
+		m_wsaBuf.buf = m_buffer;
+		m_wsaBuf.len = MAX_BUFFLEN;
+	}
+
+	//重置buf
+	void ResetBuf()
+	{
+		memset(m_buffer, 0, sizeof(m_buffer));
+	}
+
+}PER_IO_CONTEXT,*PPER_IO_CONTEXT;
+
+
+//单句柄数据
+typedef struct _PER_SOCKET_CONTEXT
+{
+	SOCKET m_socket;                           //socket
+	SOCKADDR_IN m_clientAddr;                  //客户端地址信息
+	vector<PPER_IO_CONTEXT>m_IOContextList;    //装socket上的单io数据
+
+	//初始化
+	_PER_SOCKET_CONTEXT()
+	{
+		m_socket = INVALID_SOCKET;  
+		memset(&m_clientAddr, 0, sizeof(m_clientAddr));
+	}
+
+	~_PER_SOCKET_CONTEXT()
+	{
+		if (INVALID_SOCKET != m_socket)
+		{
+			closesocket(m_socket);
+			m_socket = INVALID_SOCKET;
+		}
+		//释放所有单io数据
+		for (int i = 0; i < m_IOContextList.size(); i++)
+		{
+			delete m_IOContextList[i];
+		}
+		m_IOContextList.clear();
+	}
+
+	//得到一个新的io数据结构并返回
+	PPER_IO_CONTEXT GetNewIOContext()
+	{
+		PPER_IO_CONTEXT p = new PER_IO_CONTEXT;
+		m_IOContextList.push_back(p);
+		return p;
+	}
+
+	//移除掉某一个io数据结构
+	void RemoveContext(PPER_IO_CONTEXT p)
+	{
+		for (auto i = m_IOContextList.begin(); i != m_IOContextList.end();)
+		{
+			if (*i == p)
+			{
+				delete p;
+				p = nullptr;
+				i=m_IOContextList.erase(i);
+				break;
+			}
+			i++;
+		}
+	}
+
+
+}PER_SOCKET_CONTEXT,*PPER_SOCKET_CONTEXT;
+
+
+class CIOCPModel;
+//定义工作者线程参数
+typedef struct _THREADPARARM_WORKER
+{
+	int m_noThread;                         //线程号
+	CIOCPModel *m_IOCPModel;                //指向类的指针
+
+}THREADPARAM_WORKER,*PTHREADPARAM_WORKER;
+
+
+
+
+//iocpModel
+class CIOCPModel
+{
+public:
+	CIOCPModel();
+	~CIOCPModel();
+private:
+	int m_nPort;                                                //服务器端口
+	SOCKADDR_IN *m_serverAddr;                                  //服务器ip地址
+	int m_numThreads;                                           //线程个数
+	HANDLE m_hIOCP;                                             //完成端口句柄
+	HANDLE m_hQuitEvent;                                        //推出事件句柄
+	HANDLE * m_phWorkerThreads;                                 //工作者线程句柄指针
+	CRITICAL_SECTION m_csContextList;                           //线程同步互斥量
+	vector<PPER_IO_CONTEXT>m_clientIOContextArray;              //所有客户端的IOcontext信息
+	PPER_SOCKET_CONTEXT m_pListenContext;                       //用于监听
+	LPFN_ACCEPTEX                m_lpfnAcceptEx;                // AcceptEx 和 GetAcceptExSockaddrs 的函数指针，用于调用这两个扩展函数
+	LPFN_GETACCEPTEXSOCKADDRS    m_lpfnGetAcceptExSockAddrs;
+
+	static DWORD WINAPI WorkerThreadFun(LPVOID lpParam);        //线程函数
+
+	bool LoadSocketLab();                                       //加载套接字库
+	bool Init();                                                //初始化各种参数
+	bool InitIOCP();                                            //初始化完成端口
+	bool InitSocket();                                          //初始化socket
+	bool InitWorkerThread();                                    //初始化工作者线程
+
+	void DeInit();                                              //最后全部释放掉
+
+	bool PostAccept(PPER_IO_CONTEXT p);                         //投递accept io请求
+	bool PostRecv(PPER_IO_CONTEXT p);                           //投递recv io请求
+	bool PostSend(PPER_IO_CONTEXT p);                           //投递send io请求
+
+};
+
